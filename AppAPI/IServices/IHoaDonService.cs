cb2966ec2bca6c25b8abbd592ef2469fc5816949
dungtqph28227@fpt.edu.vn <dungtqph28227@fpt.edu.vn>
2023-09-24 20:55:37 +0700

    Thanh toan

---
+++ AppAPI/IServices/IHoaDonService.cs
@@ -0,0 +1,14 @@
+﻿using AppData.Models;
+using AppData.ViewModels;
+
+namespace AppAPI.IServices
+{
+    public interface IHoaDonService
+    {
+        public bool CreateHoaDon(List<ChiTietHoaDonViewModel> chiTietHoaDons,HoaDonViewModel hoaDon);
+        public List<HoaDon> GetAllHoaDon();
+        public List<ChiTietHoaDon> GetAllChiTietHoaDon(Guid idHoaDon);
+        public bool UpdateTrangThaiGiaoHang(Guid idHoaDon, int trangThai);
+        
+    }
+}
---
+++ AppData/ViewModels/HoaDonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using AppData.Models;
+
+namespace AppData.ViewModels
+{
+    public class HoaDonViewModel
+    {
+        public List<ChiTietHoaDonViewModel> ChiTietHoaDons { get; set; }
+        public string Ten { get; set; }
+        public string SDT { get; set; }
+        public string Email { get; set; }
+        public string PhuongThucThanhToan { get; set; }
+        public string DiaChi { get; set; }
+        public int TienShip { get; set; }
+        public Guid? IDNhanVien { get; set; }
+        public Guid? IDVoucher { get; set; }
+    }
+}
