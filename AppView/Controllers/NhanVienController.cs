bb14644b6a6278d952d625e2d1c306e2fc942e7b
Úy Lê <phonglhph24259@fpt.edu.vn>
2023-10-06 16:48:24 +0700

    giaodien vtnv

---
+++ AppView/Controllers/NhanVienController.cs
@@ -0,0 +1,89 @@
+﻿using AppData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace AppView.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        private HttpClient _httpClient;
+        public NhanVienController()
+        {
+            _httpClient = new HttpClient();
+        }
+        // GET: UserController
+        public async Task<IActionResult> Show()
+        {
+            string apiUrl = "https://localhost:7095/api/NhanVien/GetAll";
+            var response = await _httpClient.GetAsync(apiUrl);
+            string apidata = await response.Content.ReadAsStringAsync();
+            var users = JsonConvert.DeserializeObject<List<NhanVien>>(apidata);
+            return View(users);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(NhanVien nhanVien)
+        {
+            string apiUrl = $"https://localhost:7095/api/NhanVien/DangKyNhanVien?ten={nhanVien.Ten}&email={nhanVien.Email}&sdt={nhanVien.SDT}&diachi={nhanVien.DiaChi}&idVaiTro={nhanVien.IDVaiTro}&trangthai={nhanVien.TrangThai}&password={nhanVien.PassWord}";
+            var content = new StringContent(JsonConvert.SerializeObject(nhanVien), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(apiUrl, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Show");
+            }
+            return View();
+        }
+
+        // GET: UserController/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            string apiUrl = $"https://localhost:7095/api/NhanVien/{id}";
+            var response = await _httpClient.GetAsync(apiUrl);
+            string apidata = await response.Content.ReadAsStringAsync();
+
+            var user = JsonConvert.DeserializeObject<NhanVien>(apidata);
+            return View(user);
+        }
+
+
+        // GET: UserController/Edit/5
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            string apiUrl = $"https://localhost:7095/api/NhanVien/{id}";
+            var response = await _httpClient.GetAsync(apiUrl);
+            string apidata = await response.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<NhanVien>(apidata);
+            return View(user);
+        }
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid Id, NhanVien nv)
+        {
+            string apiUrl = $"https://localhost:7095/api/NhanVien/{nv.ID}?ten={nv.Ten}&email={nv.Email}&sdt={nv.SDT}&diachi={nv.DiaChi}&idVaiTro={nv.IDVaiTro}&trangthai={nv.TrangThai}&password={nv.PassWord}";
+            var content = new StringContent(JsonConvert.SerializeObject(nv), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync(apiUrl, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Show");
+            }
+            return View(nv);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit2(Guid id)
+        {
+            string apiUrl = $"https://localhost:7109/api/User/{id}";
+            var response = await _httpClient.GetAsync(apiUrl);
+            string apidata = await response.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<NhanVien>(apidata);
+            return View(user);
+        }
+    }
+}
---
+++ AppView/Controllers/VaiTroController.cs
@@ -0,0 +1,88 @@
+﻿using AppData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Text;
+
+namespace AppView.Controllers
+{
+    public class VaiTroController : Controller
+    {
+        private HttpClient _httpClient;
+        public VaiTroController()
+        {
+                _httpClient = new HttpClient();
+        }
+        public IActionResult HomePageAdmin()
+        {
+            return View();
+        }
+        
+        // GET: RoleController
+        public async Task<IActionResult> Show()
+        {
+            string apiURL = $"https://localhost:7095/api/VaiTro";
+            var response = await _httpClient.GetAsync(apiURL);
+            var apiData = await response.Content.ReadAsStringAsync();
+            var roles = JsonConvert.DeserializeObject<List<VaiTro>>(apiData);
+            return View(roles);
+        }
+
+        // GET: RoleController/Details/5
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid Id)
+        {
+            string apiURL = $"https://localhost:7095/api/VaiTro/{Id}";
+            var response = await _httpClient.GetAsync(apiURL);
+            var apiData = await response.Content.ReadAsStringAsync();
+            var roles = JsonConvert.DeserializeObject<VaiTro>(apiData);
+            return View(roles);
+        }
+
+        // GET: RoleController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RoleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(VaiTro vaiTro)
+        {
+            string apiURL = $"https://localhost:7095/api/VaiTro?ten={vaiTro.Ten}&Status={vaiTro.TrangThai = 1}";
+            var content = new StringContent(JsonConvert.SerializeObject(vaiTro), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(apiURL, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Show");
+            }
+            return View();
+        }
+        // GET: RoleController/Edit/5
+        public async Task<IActionResult> Edit(Guid Id)
+        {
+            string apiURL = $"https://localhost:7095/api/VaiTro/{Id}";
+            var response = await _httpClient.GetAsync(apiURL);
+            var apiData = await response.Content.ReadAsStringAsync();
+            var roles = JsonConvert.DeserializeObject<VaiTro>(apiData);
+            return View(roles);
+        }
+
+        // POST: RoleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid Id, VaiTro vaiTro)
+        {
+            string apiURL = $"https://localhost:7095/api/VaiTro/id={Id}&RoleName={vaiTro.Ten}&Status={vaiTro.TrangThai}";
+            var content = new StringContent(JsonConvert.SerializeObject(vaiTro), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync(apiURL, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Show");
+            }
+            return View();
+        }
+        
+    }
+}
