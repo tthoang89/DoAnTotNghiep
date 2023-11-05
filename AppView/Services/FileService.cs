bb59aabf7c8763b5bff76ee13a37aefdbbffe489
chungtam1801 <tamncph25588@fpt.edu.vn>
2023-11-05 21:27:55 +0700

    Add ChiTietSanPhamView

---
+++ AppView/Services/FileService.cs
@@ -0,0 +1,23 @@
+﻿using AppData.ViewModels.SanPham;
+using AppView.IServices;
+using Microsoft.Extensions.Hosting;
+
+namespace AppView.Services
+{
+    public class FileService : IFileService
+    {
+        public async Task<string> AddFile(IFormFile file,string wwwRootPath)
+        {
+            //string wwwrootPath = _hostEnvironment.WebRootPath;
+            string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+            string extension = Path.GetExtension(file.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            string path = Path.Combine(wwwRootPath + "/img/product/", fileName);
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            return fileName;
+        }
+    }
+}
