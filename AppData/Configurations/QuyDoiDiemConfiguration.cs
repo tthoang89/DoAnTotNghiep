e9ed26f9e1cb8334483eda216581896ae5242750
chungtam1801 <tamncph25588@fpt.edu.vn>
2023-12-15 23:35:53 +0700

    sua db

--- AppData/Configurations/QuyDoiDiemConfiguration.cs
+++ AppData/Configurations/QuyDoiDiemConfiguration.cs
@@ -13,6 +13,7 @@ namespace AppData.Configurations
             builder.Property(x => x.TiLeTichDiem).HasColumnType("int");
             builder.Property(x => x.TiLeTieuDiem).HasColumnType("int");
             builder.Property(x => x.TrangThai).HasColumnType("int");
+            builder.HasData(new QuyDoiDiem() { ID = new Guid("16BD37C4-CEF0-4E92-9BB5-511C43D99037"),TiLeTichDiem= 0,TiLeTieuDiem= 0 ,TrangThai=1});
         }
     }
 }
