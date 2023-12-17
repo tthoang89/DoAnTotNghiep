5ebaa43c97435fa7507a366747b179ee7f51dfcc
DuAnThucTapKy7 <lamtvph26027@fpt.edu.vn>
2023-12-17 15:37:47 +0700

    sua kh

---
+++ AppView/PhanTrang/PhanTrangDon.cs
@@ -0,0 +1,11 @@
+﻿using AppData.Models;
+using AppData.ViewModels;
+
+namespace AppView.PhanTrang
+{
+    public class PhanTrangDon
+    {
+        public IEnumerable<ListDon> listdon { get; set; } = new List<ListDon>();
+        public PagingInfo PagingInfo { get; set; } = new PagingInfo();
+    }
+}
