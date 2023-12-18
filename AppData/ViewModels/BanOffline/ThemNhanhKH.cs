b4ec16cabb69a51ee3edc4a4e4cc6af46f6a2b04
Nhinhph25737 <nhinhphph25737@fpt.edu.vn>
2023-12-18 18:56:23 +0700

    ngaythanhtoan

---
+++ AppData/ViewModels/BanOffline/ThemNhanhKH.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.ViewModels.BanOffline
+{
+    public class ThemNhanhKH
+    {
+        public Guid IDKhachHang { get; set; }
+        public string Ten { get; set; }
+        public int? GioiTinh { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string? DiaChi { get; set; }
+        public string? SDT { get; set; }
+    }
+}
